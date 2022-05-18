69445a1eb43bdc99602e833a05efc7d8c05d2807
Lyssie <lyssiewright.15@gmail.com>
2022-05-18 10:40:51 -0500

    commit tree 4a90fdfa0fcd086157a9ea065f12d1ead453655d

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,15 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(string)
+    @stack = []
+
+    string.chars.each do |char|
+        if char == "("
+            @stack.push(char)
+        else
+            stack.pop
+        end
+    end
+
+end
